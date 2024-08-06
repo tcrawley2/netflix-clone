2fdfca442b59cddfdfd1630fd80a1cc403884a1a
Tim Crawley Jr <timcrawley@Tims-MacBook-Pro.local>
2024-08-06 15:45:45 -0700

    refactored routes out of server.js

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,8 @@
 import express from "express";
+import authRoutes from "./routes/auth.route.js";
 const app = express();
 
-// best practice - "api/{version_number}/endpoint"
-app.get("/api/v1/signup", (req, res) => {
-  res.send("signup route");
-});
-app.get("/api/v1/login", (req, res) => {
-  res.send("login route");
-});
-app.get("/api/v1/logout", (req, res) => {
-  res.send("logout route");
-});
+app.use("/api/v1/auth", authRoutes);
 
 app.listen(5500, () => {
   console.log("Server started at http://localhost:5500");
