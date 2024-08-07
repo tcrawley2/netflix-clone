644423bf892b3b0c2b46252cfcb5a7d96f5dacb6
Tim Crawley Jr <timcrawley@Tims-MacBook-Pro.local>
2024-08-07 01:14:12 -0700

    chore: added salt functionality to hash passwords in db

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import { User } from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
 
 export async function signup(req, res) {
   try {
@@ -38,12 +39,15 @@ export async function signup(req, res) {
         .json({ success: false, message: "Username already exists." });
     }
 
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+
     const PROFILE_PICS = ["/avatar1.png", "avatar2.png", "avatar3.png"];
     const image = PROFILE_PICS[Math.floor(Math.random() * PROFILE_PICS.length)];
 
     const newUser = new User({
       email,
-      password,
+      password: hashedPassword,
       username,
       image,
     });
