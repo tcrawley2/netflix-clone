8bbbba5d5819e117def98b164abd34af868a161c
Tim Crawley Jr <timcrawley@Tims-MacBook-Pro.local>
2024-08-10 15:27:31 -0700

    added routes for fetching tv shows

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 
 import authRoutes from "./routes/auth.route.js";
 import movieRoutes from "./routes/movie.route.js";
+import tvRoutes from "./routes/tv.route.js";
 
 import { ENV_VARS } from "./config/envVars.js";
 import { connectDB } from "./config/db.js";
@@ -12,6 +13,7 @@ app.use(express.json()); // will parse req.body object
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/movie", movieRoutes);
+app.use("/api/v1/tv", tvRoutes);
 
 app.listen(PORT, () => {
   console.log("Server started at http://localhost:" + PORT);
