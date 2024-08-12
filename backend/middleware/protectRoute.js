f44981b0acf4f4ec2b3b328b223085e08b931a08
Tim Crawley Jr <timcrawley@Tims-MacBook-Pro.local>
2024-08-12 11:03:11 -0700

    added middleware to protect data-fetching routes

---
+++ backend/middleware/protectRoute.js
@@ -0,0 +1,39 @@
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model.js";
+import { ENV_VARS } from "../config/envVars.js";
+
+export const protectRoute = async (req, res, next) => {
+  try {
+    // name of token same as in ../utils/generateToken.js
+    const token = req.cookies["jwt-netflix"];
+
+    if (!token) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Unauthorized - No Token Provided" });
+    }
+
+    const decoded = jwt.verify(token, ENV_VARS.JWT_SECRET);
+
+    if (!decoded) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Unauthorized - Invalid Token" });
+    }
+    // the userId that we passed into the token
+    const user = await User.findById(decoded.userId).select("-password");
+
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    console.log("Error in protectRoute middleware: ", error.message);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
