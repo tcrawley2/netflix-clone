a89a6f54c42c99b2a291c1f653553ee9d5b26e12
Tim Crawley Jr <timcrawley@Tims-MacBook-Pro.local>
2024-08-14 18:40:06 -0700

    added Footer component to App.js to appear on all pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,18 @@ import { Routes, Route } from "react-router-dom";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/home/HomePage";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<HomePage />} />
-      <Route path='/login' element={<LoginPage />} />
-      <Route path='/signup' element={<SignUpPage />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/signup' element={<SignUpPage />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
