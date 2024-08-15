f4961607ff1341a73d742727538fc6b080e884cd
Tim Crawley Jr <timcrawley@Tims-MacBook-Pro.local>
2024-08-15 14:25:31 -0700

    created authUser store to act as global state for userAuth

---
+++ frontend/src/store/authUser.js
@@ -0,0 +1,43 @@
+// global state management
+import axios from "axios";
+import toast from "react-hot-toast";
+import { create } from "zustand";
+
+export const useAuthStore = create((set) => ({
+  user: null,
+  isSigningUp: false,
+  isCheckingAuth: true,
+  isLoggingOut: false,
+  signup: async (credentials) => {
+    set({ isSigningUp: true });
+    try {
+      const response = await axios.post("/api/v1/auth/signup", credentials);
+      set({ user: response.data.user, isSigningUp: false });
+      toast.success("Account successfully created");
+    } catch (error) {
+      toast.error(error.response.data.message || "An error occured");
+      set({ isSigningUp: false, user: null });
+    }
+  },
+  login: async () => {},
+  logout: async () => {
+    set({ isLoggingOut: true });
+    try {
+      await axios.post("/api/v1/auth/logout");
+      set({ user: null, isLoggingOut: false });
+      toast.success("Logged out successfully");
+    } catch (error) {
+      set({ isLoggingOut: false });
+      toast.error(error.response.data.message || "Logout failed");
+    }
+  },
+  authCheck: async () => {
+    set({ isCheckingAuth: true });
+    try {
+      const response = await axios.get("/api/v1/auth/authCheck");
+      set({ user: response.data.user, isCheckingAuth: false });
+    } catch (error) {
+      set({ isCheckingAuth: false, user: null });
+    }
+  },
+}));
